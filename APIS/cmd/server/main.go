0e9ee443d21f959b3ea51eadc7b0fe4e72a45bb5
Italo Gabriel <italo.pereira@pagtel.com.br>
2023-08-16 21:08:20 -0300

    api server

--- APIS/cmd/server/main.go
+++ APIS/cmd/server/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/ItaloG/Go-expert/APIS/configs"
+	"github.com/ItaloG/Go-expert/APIS/internal/entity"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	http.ListenAndServe(":8000", nil)
 }
