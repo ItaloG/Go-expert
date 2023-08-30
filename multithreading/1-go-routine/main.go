76c26540415810e59d7cc3f5eaca0861852c59c4
Italo Gabriel <Italog03@gmail.com>
2023-08-30 15:18:02 +0000

    multithreading - 2

--- multithreading/1-go-routine/main.go
+++ multithreading/1-go-routine/main.go
@@ -2,34 +2,30 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
-func task(name string, wg *sync.WaitGroup) {
+func task(name string) {
 	for i := 0; i < 10; i++ {
 		fmt.Printf("%d: Task %s is running\n", i, name)
 		time.Sleep(1 * time.Second)
-		wg.Done()
 	}
 }
 
 // Thread 1
 func main() {
-	waitGroup := sync.WaitGroup{}
-	waitGroup.Add(25)
 	// Tread 2
-	go task("A", &waitGroup)
+	go task("A")
 	// Tread 3
-	go task("B", &waitGroup)
+	go task("B")
 	// Tread 4
 	go func() {
 		for i := 0; i < 5; i++ {
 			fmt.Printf("%d: Task %s is running\n", i, "anonymous")
 			time.Sleep(1 * time.Second)
-			waitGroup.Done()
 		}
 	}()
-
-	waitGroup.Wait()
+	// Nada aqui.
+	// sair
+	time.Sleep(15 * time.Second)
 }
--- multithreading/2-wait-groups/main.go
+++ multithreading/2-wait-groups/main.go
@@ -2,30 +2,34 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func task(name string) {
+func task(name string, wg *sync.WaitGroup) {
 	for i := 0; i < 10; i++ {
 		fmt.Printf("%d: Task %s is running\n", i, name)
 		time.Sleep(1 * time.Second)
+		wg.Done()
 	}
 }
 
 // Thread 1
 func main() {
+	waitGroup := sync.WaitGroup{}
+	waitGroup.Add(25)
 	// Tread 2
-	go task("A")
+	go task("A", &waitGroup)
 	// Tread 3
-	go task("B")
+	go task("B", &waitGroup)
 	// Tread 4
 	go func() {
 		for i := 0; i < 5; i++ {
 			fmt.Printf("%d: Task %s is running\n", i, "anonymous")
 			time.Sleep(1 * time.Second)
+			waitGroup.Done()
 		}
 	}()
-	// Nada aqui.
-	// sair
-	time.Sleep(15 * time.Second)
+
+	waitGroup.Wait()
 }
